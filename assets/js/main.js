183330686f8f9a14575fcf9929b9824911029aad
Frens1999 <frenskadiasi@gmail.com>
2021-12-17 01:24:50 +0100

    Create blog section on home

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,36 @@
     }
   }
 
+
+  // TESTIMONIALS
+
+
+  new Swiper('.testimonials-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
   /**
    * Easy on scroll event listener 
    */
